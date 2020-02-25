69f350f7b875e052181c9f9c3067059b6ef89ed1
Daniel Westerlund <westerlund.d@gmail.com>
2020-02-25 14:34:23 +0100

    Add random graph test with negative weights

--- test/test_random_graph.py
+++ test/test_random_graph.py
@@ -28,11 +28,24 @@ class TestRandomGraph(unittest.TestCase):
         distance = pow(char_path_length-char_path_length_mean,2)/char_path_length_std
         self.assertLess(distance, 0.35)
 
-    def test_wd(self):
+    def test_wd1(self):
         measure_list = MeasureParser.list_measures()
         A=np.array([[0.,1.,1.,0.5,0.],
                     [1.,0.,0.,1.,0.],
                     [0.,1.,0.,1.,1.],
+                    [0.,1.,0.,0.,1.],
+                    [1.,0.,1.,0.5,1.]])
+        graph = GraphWD(A, measure_list[GraphWD])
+        random = graph.get_random_graph()
+        self.assertTrue(random.A.shape == A.shape)
+        self.assertEqual(np.max(random.A), 1)
+        self.assertEqual(np.min(random.A), 0)
+
+    def test_wd2(self):
+        measure_list = MeasureParser.list_measures()
+        A=np.array([[0.,2.,2.,0.5,0.],
+                    [1.,0.,0.,1.,0.],
+                    [0.,1.,0.,-1.,1.],
                     [0.,1.,0.,0.,1.],
                     [1.,0.,1.,0.5,1.]])
         graph = GraphWD(A, measure_list[GraphWD])
