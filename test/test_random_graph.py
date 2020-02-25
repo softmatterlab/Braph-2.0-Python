b613ac5ceb78a791852775d36719c4cf271dc02b
Daniel Westerlund <westerlund.d@gmail.com>
2020-02-25 09:55:07 +0100

    Add tests for random graph

--- test/test_random_graph.py
+++ test/test_random_graph.py
@@ -9,12 +9,32 @@ class TestRandomGraph(unittest.TestCase):
         A=np.array([[0,1,1,0],[1,0,0,1],[0,1,0,1],[0,1,0,0]])
         graph = GraphBU(A, measure_list[GraphBU], 'zero', 'max')
         random = graph.get_random_graph()
-    
+        self.assertTrue(np.array_equal(random.A, np.array([[0,1,1,0],
+                                                           [1,0,1,1],
+                                                           [1,1,0,1],
+                                                           [0,1,1,0]])))
+
     def test_bd(self):
         measure_list = MeasureParser.list_measures()
         A=np.array([[0,1,1,0,0],[1,0,0,1,0],[0,1,0,1,1],[0,1,0,0,1],[1,0,1,0,1]])
         graph = GraphBD(A, measure_list[GraphBD])
         random = graph.get_random_graph()
+        self.assertTrue(random.A.shape == A.shape)
+        self.assertEqual(np.sum(random.A), np.sum(A))
+
+    def test_wd(self):
+        measure_list = MeasureParser.list_measures()
+        A=np.array([[0.,1.,1.,0.5,0.],
+                    [1.,0.,0.,1.,0.],
+                    [0.,1.,0.,1.,1.],
+                    [0.,1.,0.,0.,1.],
+                    [1.,0.,1.,0.5,1.]])
+        graph = GraphWD(A, measure_list[GraphWD])
+        random = graph.get_random_graph()
+        self.assertTrue(random.A.shape == A.shape)
+        self.assertEqual(np.max(random.A), 1)
+        self.assertEqual(np.min(random.A), 0)
+
     '''
     def test(self):
         measure_list = MeasureParser.list_measures()
