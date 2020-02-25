a58fd0c6ce232eb63930de3d1cb3987747aafca4
Lisa Sjöblom <sjoblomlisa@icloud.com>
2020-02-25 08:09:08 +0100

    Add random graph for bd

--- test/test_random_graph.py
+++ test/test_random_graph.py
@@ -4,12 +4,17 @@ from braphy.graph_measures.measure_parser import MeasureParser
 import numpy as np
 
 class TestRandomGraph(unittest.TestCase):
-    def test1(self):
+    def test_bu(self):
         measure_list = MeasureParser.list_measures()
         A=np.array([[0,1,1,0],[1,0,0,1],[0,1,0,1],[0,1,0,0]])
-        graph_bu = GraphBU(A, measure_list[GraphBU], 'zero', 'max')
-        random = graph_bu.get_random_graph()
-        print(random)
+        graph = GraphBU(A, measure_list[GraphBU], 'zero', 'max')
+        random = graph.get_random_graph()
+    
+    def test_bd(self):
+        measure_list = MeasureParser.list_measures()
+        A=np.array([[0,1,1,0,0],[1,0,0,1,0],[0,1,0,1,1],[0,1,0,0,1],[1,0,1,0,1]])
+        graph = GraphBD(A, measure_list[GraphBD])
+        random = graph.get_random_graph()
     '''
     def test(self):
         measure_list = MeasureParser.list_measures()
