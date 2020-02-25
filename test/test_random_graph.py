494bdd15835eb8f08582b02c62d0bd9a318325ff
Daniel Westerlund <westerlund.d@gmail.com>
2020-02-25 13:29:30 +0100

    Extend random graph tests

--- test/test_random_graph.py
+++ test/test_random_graph.py
@@ -1,6 +1,7 @@
 import unittest
 from braphy.graph import *
 from braphy.graph_measures.measure_parser import MeasureParser
+from braphy.graph_measures.measure_path_length import MeasurePathLength
 import numpy as np
 
 class TestRandomGraph(unittest.TestCase):
@@ -21,6 +22,11 @@ class TestRandomGraph(unittest.TestCase):
         random = graph.get_random_graph()
         self.assertTrue(random.A.shape == A.shape)
         self.assertEqual(np.sum(random.A), np.sum(A))
+        char_path_length_std = 0.1259
+        char_path_length_mean = 1+(1/3)
+        char_path_length = random.get_measure(MeasurePathLength, 'char_path_length')
+        distance = pow(char_path_length-char_path_length_mean,2)/char_path_length_std
+        self.assertLess(distance, 0.35)
 
     def test_wd(self):
         measure_list = MeasureParser.list_measures()
@@ -35,17 +41,5 @@ class TestRandomGraph(unittest.TestCase):
         self.assertEqual(np.max(random.A), 1)
         self.assertEqual(np.min(random.A), 0)
 
-    '''
-    def test(self):
-        measure_list = MeasureParser.list_measures()
-        A = np.array([[0, 1, 1, 1, 0, 1, 0, 0, 0, 1], [0, 1, 0, 0, 0, 1, 0, 0, 1, 0],
-                      [0, 0, 0, 1, 0, 0, 0, 1, 1, 0], [1, 0, 1, 0, 1, 0, 1, 1, 1, 0],
-                      [1, 1, 0, 0, 1, 1, 0, 1, 1, 1], [1, 0, 1, 1, 1, 0, 0, 0, 0, 0],
-                      [1, 1, 0, 0, 1, 1, 0, 1, 1, 1], [0, 1, 0, 1, 1, 1, 0, 1, 1, 0],
-                      [0, 1, 1, 0, 1, 1, 1, 0, 1, 0], [1, 0, 1, 0, 1, 1, 0, 1, 1, 0]])
-        graph_bu = GraphBU(A, measure_list[GraphBU], 'zero', 'max')
-        random = graph_bu.get_random_graph()
-        print(random)
-    '''
 if __name__ == '__main__':
     unittest.main()
