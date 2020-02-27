3fd3c0755677dca67fe5571c838e17fc36c1aada
Syntronic <dagexa@syntronic.com>
2020-02-27 13:39:16 +0100

    Initial tests for small worldness

--- braphy/graph_measures/measure_small_worldness.py
+++ braphy/graph_measures/measure_small_worldness.py
@@ -22,19 +22,27 @@ class MeasureSmallWorldness(Measure):
         if graph.is_directed():
             L = graph.get_measure(MeasurePathLength, 'char_path_length')
         else:
-            pass
+            tmp = graph.get_measure(MeasurePathLength, 'path_length')
+            res = np.mean(tmp[np.isfinite(tmp)])
+            L = res
 
-        Cr = np.zeros(M)
-        Lr = np.zeros(M)
+        Cr = np.empty((M, graph.A.shape[1]))
+        print(Cr.shape)
+        Lr = np.empty((M, graph.A.shape[1]))
 
         for i in range(M):
             gr = graph.get_random_graph()
+            print(gr.get_measure(MeasureCluster, 'cluster'))
             Cr[i] = gr.get_measure(MeasureCluster, 'cluster')
 
             if graph.is_directed():
                 Lr[i] = gr.get_measure(MeasurePathLength, 'char_path_length')
             else:
-                Lr[i] = gr.get_measure(MeasurePathLength, 'CPl_WSG?????') 
+                #Lr[i] = gr.get_measure(MeasurePathLength, 'CPl_WSG?????')
+                tmp = gr.get_measure(MeasurePathLength, 'path_length')
+                res = np.mean(tmp[np.isfinite(tmp)])
+                Lr[i] = res
+                #print(res)
                 #################### matlab code for wsg
                 #   case Graph.CPL_WSG
                 #   tmp = g.pl();
@@ -43,7 +51,8 @@ class MeasureSmallWorldness(Measure):
         Cr = np.mean(Cr)
         Lr = np.mean(Lr)
 
-        graph.measure_dict[MeasureSmallWorldness]['small_worldness'] = (C/Cr)/(L/Lr) 
+        graph.measure_dict[MeasureSmallWorldness]['small_worldness'] = (C/Cr)/(L/Lr)
+        print((C/Cr)/(L/Lr))
 
     def get_valid_graph_types():
         graph_type_measures = {}
--- test/test_small_worldness.py
+++ test/test_small_worldness.py
@@ -5,12 +5,21 @@ import numpy as np
 
 class TestSmallWorldness(unittest.TestCase):
     
-    def test1(self):
+    def test_graphBD(self):
         
         measure_list = MeasureParser.list_measures()
-        A=np.array([[0,1,1,0],[1,0,0,1],[0,1,0,1],[0,1,0,0]])
+        A=np.array([[0,1,1,0,1],[1,0,1,0,1],[0,1,1,0,1],[0,1,0,1,0],[1,0,1,0,0]])
         graph_bd = GraphBD(A, measure_list[GraphBD], 'zero')
         print(graph_bd.get_measure(MeasureSmallWorldness, 'small_worldness'))
+        self.assertTrue(1,1)
+
+
+    def test_graphBU(self):
+        
+        measure_list = MeasureParser.list_measures()
+        A=np.array([[0,1,1,0,1],[1,0,1,0,1],[0,1,1,0,1],[0,1,0,1,0],[1,0,1,0,0]])
+        graph_bu = GraphBU(A, measure_list[GraphBU], 'zero')
+        print(graph_bu.get_measure(MeasureSmallWorldness, 'small_worldness'))
         self.assertTrue(1,1)
 
 if __name__ == '__main__':
