446a56c8d354b876481637f6f9570d1e77af0b7e
Daniel Westerlund <westerlund.d@gmail.com>
2020-03-17 14:24:51 +0100

    Add SubjectfMRI load from xml

--- braphy/cohort/subjects/subject_fMRI.py
+++ braphy/cohort/subjects/subject_fMRI.py
@@ -1,6 +1,8 @@
 from braphy.cohort.subjects.subject import Subject
 from braphy.cohort.data_types.data_scalar import DataScalar
 from braphy.cohort.data_types.data_functional import DataFunctional
+import xml.etree.ElementTree as ET
+import numpy as np
 
 class SubjectfMRI(Subject):
     def __init__(self, id = 'sub_id'):
@@ -9,3 +11,28 @@ class SubjectfMRI(Subject):
     def init_data_dict(self):
         self.data_dict['age'] = DataScalar()
         self.data_dict['fMRI'] = DataFunctional()
+
+    def from_txt(file_txt):
+        raise Exception("Not implemented")
+
+    def from_xml(file_xml):
+        subjects = []
+        with open(file_xml, 'r') as f:
+            tree = ET.parse(f)
+            root = tree.getroot()
+            for item in root.findall('fMRICohort/fMRISubject'):
+                item = item.attrib
+                subject_id = item['code']
+                subject = SubjectfMRI(id = subject_id)
+
+                fmri_data = []
+                data = item['data'].strip('[]')
+                for v in data.split(";"):
+                    fmri_data.append(v.split())
+                fmri_data = np.array(fmri_data).astype(float)
+                subject.data_dict['fMRI'].set_value(fmri_data)
+                subjects.append(subject)
+        return subjects
+
+    def from_xlsx(file_xlsx):
+        raise Exception("Not implemented")
--- braphy/test/test_cohort.py
+++ braphy/test/test_cohort.py
@@ -7,12 +7,15 @@ from braphy.test.test_utility import TestUtility
 
 class TestCohort(TestUtility):
 
-    def test_read_cohort(self):
+    def test_read_mri(self):
         cohort = Cohort('mri', SubjectMRI)
         cohort.load_from_txt(file_name='gr1_MRI.txt')
         cohort.load_from_xml(file_name='gr1_MRI.xml')
         cohort.load_from_xlsx(file_name='gr1_MRI.xlsx')
 
+    def test_read_fmri(self):
+        cohort = Cohort('fmri', SubjectfMRI)
+        cohort.load_from_xml(file_name='gr1_fMRI.xml')
 
 if __name__ == '__main__':
     unittest.main()
