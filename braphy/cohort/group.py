c3e9bb2951a2697b3e350ab31dc79c4de7e3b482
Daniel Westerlund <westerlund.d@gmail.com>
2020-03-22 14:07:57 +0100

    Add cohort group comparison

--- braphy/cohort/group.py
+++ braphy/cohort/group.py
@@ -1,5 +1,6 @@
 import numpy as np
 from braphy.cohort.subjects import *
+from braphy.utility.stat_functions import StatFunctions as stat
 import statistics
 
 class Group:
@@ -58,3 +59,41 @@ class Group:
         if values.size == 0:
             return []
         return values.std(0)
+
+    def comparison(self, other, permutations = 1000):
+        assert isinstance(other, Group), "{} is not a group".format(other.__class__.__name__)
+        assert self.subject_class == other.subject_class, "{} and {} are different subject classes".format(self.subject_class.__name__, other.subject_class.__name__)
+        assert len(self.subjects) > 0, "{} has no subjects".format(self.name)
+        assert len(other.subjects) > 0, "{} has no subjects".format(other.name)
+
+        values_self = np.array([subject.data_dict['data'].value for subject in self.subjects])
+        values_other = np.array([subject.data_dict['data'].value for subject in other.subjects])
+
+        subject_length_self = np.shape(values_self)[0]
+        data_length_self = np.shape(values_self)[1]
+        data_length_other = np.shape(values_other)[1]
+        assert data_length_self == data_length_other, "Subject data length does not match: {} and {}".format(data_length_self, data_length_other)
+
+        std_self = values_self.std(0)
+        std_other = values_other.std(0)
+        mean_self = values_self.mean(0)
+        mean_other = values_other.mean(0)
+
+        values_all = np.concatenate((values_self, values_other))
+
+        diffs = []
+        for _ in range(permutations):
+            np.random.shuffle(values_all)
+            all_self = values_all[:subject_length_self,:]
+            all_other = values_all[subject_length_self:,:]
+            all_diff = all_self.mean(0) - all_other.mean(0)
+            diffs.append(all_diff)
+        diffs = np.array(diffs)
+        p_value_single = stat.p_value_one_tail(mean_self, diffs)
+        p_value_double = stat.p_value_two_tail(mean_self, diffs)
+
+        averages = [mean_self, mean_other]
+        stds = [std_self, std_other]
+        p_values = [p_value_single, p_value_double]
+
+        return averages, stds, p_values
---
+++ braphy/test/test_group.py
@@ -0,0 +1,35 @@
+import unittest
+from braphy.cohort.group import Group
+from braphy.cohort.subjects.subject_MRI import SubjectMRI
+from braphy.cohort.subjects.subject_fMRI import SubjectfMRI
+from braphy.utility.helper_functions import abs_path_from_relative
+from braphy.test.test_utility import TestUtility
+
+class TestGroup(TestUtility):
+    def test_comparison_different_classes(self):
+        Group1 = Group(SubjectMRI)
+        Group2 = SubjectMRI()
+        self.assertRaises(AssertionError, Group1.comparison, Group2)
+
+    def test_comparison_different_subject_classes(self):
+        Group1 = Group(SubjectMRI)
+        Group2 = Group(SubjectfMRI)
+        self.assertRaises(AssertionError, Group1.comparison, Group2)
+
+    def test_comparison_no_subjects(self):
+        Group1 = Group(SubjectMRI)
+        Group2 = Group(SubjectMRI)
+        self.assertRaises(AssertionError, Group1.comparison, Group2)
+
+    def test_comparison(self):
+        f1 = abs_path_from_relative(__file__, '../cohort/gr1_MRI.txt')
+        f2 = abs_path_from_relative(__file__, '../cohort/gr2_MRI.txt')
+        subjects1 = SubjectMRI.from_txt(f1)
+        subjects2 = SubjectMRI.from_txt(f2)
+
+        Group1 = Group(SubjectMRI, subjects=subjects1)
+        Group2 = Group(SubjectMRI, subjects=subjects2)
+        Group1.comparison(Group2, permutations = 100)
+
+if __name__ == '__main__':
+    unittest.main()
