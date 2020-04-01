abb73cc45df5428050b1a39ea73eaf4cab9f0a0d
Alice Deimante Neimantaite <deimante@live.se>
2020-04-01 14:44:29 +0200

    Skeleton for graph analysis

--- braphy/gui/graph_analysis_gui.py
+++ braphy/gui/graph_analysis_gui.py
@@ -14,13 +14,127 @@ class GraphAnalysis(QtWidgets.QMainWindow, Ui_MainWindow):
         QtWidgets.QMainWindow.__init__(self, parent = None)
         self.setupUi(self)
         self.init_buttons()
+        self.init_actions()
+
 
     def init_buttons(self):
-       self.btnEdit.clicked.connect(self.edit_community)
+        self.btnSelectCohort.clicked.connect(self.select_cohort)
+        self.btnEdit.clicked.connect(self.edit_community)
+        self.btnDefault.clicked.connect(self.default)
+        self.btnSubgraphAnalysis.clicked.connect(self.subgraph_analysis)
+        self.btnStartAnalysis.clicked.connect(self.start_analysis)
+
+        self.radioButtonGroup.toggled.connect(self.analyse_group)
+        self.radioButtonSubject.toggled.connect(self.analyse_subject)
+        self.radioButtonWeighted.toggled.connect(self.weighted_correlation)
+        self.radioButtonHistogram.toggled.connect(self.histogram)
+        self.radioButtonSubject.toggled.connect(self.analyse_subject)
+        self.radioButtonDensity.toggled.connect(self.binary_correlation_density)
+        self.radioButtonThreshold.toggled.connect(self.binary_correlation_threshold)
+
+        self.checkBoxRearrange.stateChanged.connect(self.rearrange)
+        self.checkBoxDivide.stateChanged.connect(self.divide)
+
+
+    def init_actions(self):
+        self.actionOpen.triggered.connect(self.open)
+        self.actionSave.triggered.connect(self.save)
+        self.actionSave_as.triggered.connect(self.save_as)
+        self.actionImport_xml.triggered.connect(self.import_xml)
+        self.actionExport_xml.triggered.connect(self.export_xml)
+        self.actionClose.triggered.connect(self.close)
+
+        self.actionGenerate_figure.triggered.connect(self.generate_figure)
+
+        self.actionAbout.triggered.connect(self.about)
+
+        self.actionZoom_in.triggered.connect(self.zoom_in)
+        self.actionZoom_out.triggered.connect(self.zoom_out)
+        self.actionPan.triggered.connect(self.pan)
+        self.actionPan_x_y.triggered.connect(self.pan_x_y)
+        self.actionInsert_colorbar.triggered.connect(self.insert_colorbar)
+
+    def select_cohort(self):
+        pass
 
     def edit_community(self):
         self.community_structure_gui = CommunityStructure(self)
         self.community_structure_gui.show()
+
+    def default(self):
+        pass
+
+    def subgraph_analysis(self):
+        pass
+
+    def start_analysis(self):
+        pass
+
+    def analyse_group(self):
+        pass
+
+    def analyse_subject(self):
+        pass
+
+    def weighted_correlation(self):
+        pass
+
+    def histogram(self):
+        pass
+
+    def analyse_subject(self):
+        pass
+
+    def binary_correlation_density(self):
+        pass
+
+    def binary_correlation_threshold(self):
+        pass
+
+    def rearrange(self):
+        pass
+
+    def divide(self):
+        pass
+
+    def open(self):
+        pass
+
+    def save(self):
+        pass
+
+    def save_as(self):
+        pass
+
+    def import_xml(self):
+        pass
+
+    def export_xml(self):
+        pass
+
+    def close(self):
+        pass
+
+    def generate_figure(self):
+        pass
+
+    def about(self):
+        pass
+
+    def zoom_in(self):
+        pass
+
+    def zoom_out(self):
+        pass
+
+    def pan(self):
+        pass
+
+    def pan_x_y(self):
+        pass
+
+    def insert_colorbar(self):
+        pass
 
 def run():
     app = QtWidgets.QApplication(sys.argv)
