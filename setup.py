fc80592c7addd74f0a5594b80eba5c45f6dd5374
Daniel Westerlund <westerlund.d@gmail.com>
2020-05-06 10:21:11 +0200

    Specify required package versions

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(
     description='A graph theory software for the analysis of brain connectivity',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=['openpyxl', 'pandas', 'pyopengl', 'pyqtgraph', 'numpy', 'xlrd', 'matplotlib', 'pyqt5==5.13'],
+    install_requires=['openpyxl>=3.0.3', 'pandas>=1.0.2', 'pyopengl>=3.1.5', 'pyqtgraph>=0.10.0', 'numpy>=1.18', 'xlrd>=1.2.0', 'matplotlib>=3.2.1', 'pyqt5==5.13'],
 )
