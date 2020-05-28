58ebd35edf82d5718be6b2cd5bd8c41777d4b318
Lisa Sjöblom <sjoblomlisa@icloud.com>
2020-05-28 10:45:18 +0200

    Implement selection and remove buttons

--- braphy/gui/widgets/measures_widget.py
+++ braphy/gui/widgets/measures_widget.py
@@ -11,6 +11,7 @@ class MeasuresWidget(Base, Form):
         super(MeasuresWidget, self).__init__(parent)
         self.setupUi(self)
         self.init_buttons()
+        self.tableWidget.setSelectionBehavior(QAbstractItemView.SelectRows)
 
     def init(self, measure_type, analysis):
         self.analysis = analysis
@@ -22,8 +23,8 @@ class MeasuresWidget(Base, Form):
         self.measure()
 
     def init_buttons(self):
-        self.btnSelectAll.clicked.connect(self.select_all)
-        self.btnClearSelection.clicked.connect(self.clear_selection)
+        self.btnSelectAll.clicked.connect(self.tableWidget.selectAll)
+        self.btnClearSelection.clicked.connect(self.tableWidget.clearSelection)
         self.btnRemove.clicked.connect(self.remove)
 
         self.btnMeasure.clicked.connect(self.measure)
@@ -42,14 +43,19 @@ class MeasuresWidget(Base, Form):
         for label in self.analysis.cohort.atlas.get_brain_region_labels():
             self.comboBoxRegion.addItem(label)
 
-    def select_all(self):
-        pass
-
-    def clear_selection(self):
-        pass
-
     def remove(self):
-        pass
+        selected = self.get_selected()
+        for i in range(len(selected) - 1, -1, -1):
+            if self.btnMeasure.isChecked():
+                index_to_remove = self.measurement_index_mapping[selected[i]]
+                del self.analysis.measurements[index_to_remove]
+            elif self.btnComparison.isChecked():
+                index_to_remove = self.comparison_index_mapping[selected[i]]
+                del self.analysis.comparisons[index_to_remove]
+            elif self.btnRandomComparison.isChecked():
+                index_to_remove = self.random_comparison_index_mapping[selected[i]]
+                del self.analysis.random_comparisons[index_to_remove]
+        self.update_table()
 
     def measure(self):
         self.comboBoxGroup2.setEnabled(False)
@@ -85,9 +91,11 @@ class MeasuresWidget(Base, Form):
         if self.btnMeasure.isChecked():
             self.tableWidget.setColumnCount(5)
             self.tableWidget.setHorizontalHeaderLabels(['Value', 'Notes', 'Measure', 'Group', 'Param'])
-            for measurement in self.analysis.measurements:
+            self.measurement_index_mapping = {}
+            for i, measurement in enumerate(self.analysis.measurements):
                 if measurement.is_global() and current_group_1 == measurement.group:
                     row = self.tableWidget.rowCount()
+                    self.measurement_index_mapping[row] = i
                     self.tableWidget.setRowCount(row + 1)
                     item = QTableWidgetItem(float_to_string(measurement.value))
                     self.tableWidget.setItem(row, 0, item)
@@ -102,9 +110,11 @@ class MeasuresWidget(Base, Form):
         elif self.btnComparison.isChecked():
             self.tableWidget.setColumnCount(12)
             self.tableWidget.setHorizontalHeaderLabels(['Difference', 'p (1-tailed)', 'p (2-tailed)', 'Value 1', 'Value 2', 'CI lower', 'CI upper', 'Notes', 'Measure', 'Group 1', 'Group 2', 'Param'])
-            for comparison in self.analysis.comparisons:
+            self.comparison_index_mapping = {}
+            for i, comparison in enumerate(self.analysis.comparisons):
                 if comparison.measure_class.is_global(comparison.sub_measure) and current_group_1 == comparison.groups[0] and current_group_2 == comparison.groups[1]:
                     row = self.tableWidget.rowCount()
+                    self.comparison_index_mapping[row] = i
                     self.tableWidget.setRowCount(row + 1)
                     item = QTableWidgetItem(float_to_string(comparison.measures[1] - comparison.measures[0]))
                     self.tableWidget.setItem(row, 0, item)
@@ -134,11 +144,13 @@ class MeasuresWidget(Base, Form):
             self.tableWidget.setColumnCount(10)
             self.tableWidget.setHorizontalHeaderLabels(['Comp value', 'p (1-tailed)', 'p (2-tailed)', 'Real value', 'CI lower', 'CI upper', 'Notes', 'Measure', 'Group', 'Param'])
 
-
-
     def update_nodal_table(self):
         pass
 
     def update_binodal_table(self):
         pass
+
+    def get_selected(self):
+        selected = [item.row() for item in self.tableWidget.selectionModel().selectedRows()]
+        return selected
 
