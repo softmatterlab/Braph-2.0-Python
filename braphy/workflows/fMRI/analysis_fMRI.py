129a5936c76a79c7ec6fe04a5d61a9ac241be1f5
Lisa Sjöblom <sjoblomlisa@icloud.com>
2020-06-01 13:27:01 +0200

    Add calculation of comparison for fmri

--- braphy/workflows/fMRI/analysis_fMRI.py
+++ braphy/workflows/fMRI/analysis_fMRI.py
@@ -22,7 +22,37 @@ class AnalysisfMRI(Analysis):
         pass
 
     def calculate_comparison(self, measure_class, sub_measure, groups, permutations = 1000):
-        pass
+        group_1 = self.cohort.groups[groups[0]]
+        group_2 = self.cohort.groups[groups[1]]
+        measures_1 = np.array(self.get_measurement(measure_class, sub_measure, groups[0]).get_value())
+        measures_2 = np.array(self.get_measurement(measure_class, sub_measure, groups[1]).get_value())
+        print(len(measures_1))
+        print(len(measures_2))
+        measures = np.concatenate((measures_1, measures_2))
+        print(len(measures))
+        permutation_diffs = []
+        for _ in range(permutations):
+            np.random.shuffle(measures)
+            permutated_measures_1 = np.array(measures[:len(measures_1)])
+            permutated_measures_2 = np.array(measures[len(measures_1):])
+
+            mean_permutated_1 = np.mean(permutated_measures_1, axis = 0)
+            mean_permutated_2 = np.mean(permutated_measures_2, axis = 0)
+            print(mean_permutated_2.shape)
+
+            permutation_diffs.append(mean_permutated_2 - mean_permutated_1)
+
+        permutation_diffs = np.array(permutation_diffs)
+        print(permutation_diffs.shape)
+        difference_mean = np.mean(measures_2, axis = 0) - np.mean(measures_1, axis = 0)
+        print(difference_mean.shape)
+        p1 = stat.p_value(difference_mean, permutation_diffs, True)
+        p2 = stat.p_value(difference_mean, permutation_diffs, False)
+        percentiles = None #stat.quantiles(difference_mean, 100)
+
+        comparison = ComparisonfMRI(groups, measure_class, sub_measure, permutation_diffs, (p1, p2), (0, 0), (measures_1, measures_2), permutations)
+        print(p1, p2)
+        return comparison
 
     def get_graph(self, group_index):
         A = self.get_correlation(group_index)
