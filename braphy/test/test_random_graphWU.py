23875d64188b0424698daf2f9d623c1de52c559f
Alice Deimante Neimantaite <deimante@live.se>
2020-06-03 14:21:24 +0200

    Added random graphWU and test

--- braphy/test/test_random_graphWU.py
+++ braphy/test/test_random_graphWU.py
@@ -3,10 +3,10 @@ from braphy.graph.graph_factory import GraphFactory, GraphSettings
 from braphy.test.test_utility import TestUtility
 import numpy as np
 
-class TestRandomGraphWD(unittest.TestCase):
+class TestRandomGraphWU(unittest.TestCase):
     def test_graph(self):
-        A = np.array([[0.,0.5,0.,0.3],[0.7,0.,0.1,0.],[0.,0.,0.,0.1],[0.,0.,0.,0.]])
-        settings = GraphSettings.get_wd()
+        A = np.array([[0.,0.5,0.,0.],[0.5,0.,0.,0.2],[0.,0.,0.,0.1],[0.,0.2,0.1,0.]])
+        settings = GraphSettings.get_wu()
         graph = GraphFactory.get_graph(A, settings)
         random = graph.get_random_graph(number_of_weights=1)
         self.assertTrue(random.shape == A.shape)
