14985f66d8531a5bc259318c991111946a6434e0
Daniel Westerlund <westerlund.d@gmail.com>
2020-07-13 11:58:49 +0200

    Add kill braphy

--- install.sh
+++ install.sh
@@ -16,9 +16,26 @@ cat > $application_file << EOL
 Type=Application
 Terminal=false
 Exec=$(pwd)/bin/braphy.sh
+Icon=$(pwd)/braphy/gui/icons/application_icon.png
 Name=braphy
 EOL
 
 chmod +x $application_file
+
+application_filename_kill=kill_braphy.desktop
+application_kill=$application_directory/$application_filename_kill
+
+cat > $application_kill << EOL
+#!/usr/bin/env xdg-open
+
+[Desktop Entry]
+Type=Application
+Terminal=false
+Exec=$(pwd)/bin/kill_braphy.sh
+Icon=$(pwd)/braphy/gui/icons/application_icon_kill.png
+Name=kill braphy
+EOL
+
+chmod +x $application_kill
 
 popd
