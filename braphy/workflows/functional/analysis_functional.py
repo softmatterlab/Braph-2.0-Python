cc95fd25afe7ee35cdea1137f3805dbc87fc5633
Lisa Sjöblom <sjoblomlisa@icloud.com>
2020-08-12 15:30:25 +0200

    Add calculate random comparison for functional data

--- braphy/workflows/functional/analysis_functional.py
+++ braphy/workflows/functional/analysis_functional.py
@@ -1,6 +1,7 @@
 from braphy.analysis.analysis import Analysis
 from braphy.workflows.functional.measurement_functional import MeasurementFunctional
 from braphy.workflows.functional.comparison_functional import ComparisonFunctional
+from braphy.workflows.functional.random_comparison_functional import RandomComparisonFunctional
 from braphy.graph.measures.measure_community_structure import MeasureCommunityStructure
 from braphy.utility.permutation import Permutation
 from braphy.utility.stat_functions import StatFunctions as stat
@@ -32,8 +33,38 @@ class AnalysisFunctional(Analysis):
         measurement = MeasurementFunctional(group_index, measure_class, sub_measure, values, self.graph_settings.value_binary)
         return measurement
 
-    def calculate_random_comparison(self, measure_class, sub_measure, group):
-        pass
+    def calculate_random_comparison(self, measure_class, sub_measure, group_index,
+                                    randomization_number, number_of_weights, attempts_per_edge):
+        graphs = self.get_graph(group_index)
+        measures = np.array(self.get_measurement(measure_class, sub_measure, group_index).get_value())
+        mean_measures = np.mean(measures, axis = 0)
+        differences = []
+        mean_random_measures = 0
+        for _ in range(randomization_number):
+            random_measures = []
+            for graph in graphs:
+                random_A = graph.get_random_graph(attempts_per_edge, number_of_weights)
+                random_graph = GraphFactory.get_graph(random_A, self.graph_settings)
+                random_measure = np.array(random_graph.get_measure(measure_class, sub_measure, save = False))
+                random_measures.append(random_measure)
+            differences.append(mean_measures - np.mean(random_measures, axis = 0))
+            mean_random_measures += np.mean(random_measures, axis = 0)
+
+        mean_random_measures = mean_random_measures / randomization_number
+        difference = mean_measures - mean_random_measures
+        differences = np.array(differences)
+        p1 = stat.p_value(difference, differences, True)
+        p2 = stat.p_value(difference, differences, False)
+        quantiles = stat.quantiles(differences, 41)
+        CI_lower = quantiles[1]
+        CI_upper = quantiles[39]
+
+        random_comparison = RandomComparisonFunctional(group_index, measure_class, sub_measure,
+                                                       attempts_per_edge, number_of_weights,
+                                                       randomization_number, mean_measures, mean_random_measures,
+                                                       difference, differences, (p1, p2),
+                                                       (CI_lower, CI_upper), self.graph_settings.value_binary)
+        return random_comparison
 
     def calculate_comparison(self, measure_class, sub_measure, groups, permutations = 1000, longitudinal = False):
         group_1 = self.cohort.groups[groups[0]]
