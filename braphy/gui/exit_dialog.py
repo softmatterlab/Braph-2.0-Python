c623555ce7147eb95eb957f43265870f4d4819de
SysterLyster <linnlyster@hotmail.com>
2020-08-26 11:38:31 +0200

    My first commit ever in BRAPH.

--- braphy/gui/exit_dialog.py
+++ braphy/gui/exit_dialog.py
@@ -3,7 +3,7 @@ from PyQt5 import QtWidgets
 class ExitDialog(QtWidgets.QMainWindow):
 
     def closeEvent(self, event):
-        reply = QtWidgets.QMessageBox.question(self, 'Message', "Are you sure you want to quit?",
+        reply = QtWidgets.QMessageBox.question(self, 'Message', "Are you sure you want to quit, buddy?",
                                                QtWidgets.QMessageBox.Yes,
                                                QtWidgets.QMessageBox.Cancel)
         if reply == QtWidgets.QMessageBox.Yes:
